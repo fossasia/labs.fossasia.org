61b5a7c92f23927bf3c348b45d05982846bbea82
Aishh2k <aiswarya2000may@gmail.com>
2018-12-21 23:38:57 +0530

    Edits

---
+++ js/backtotop.js
@@ -0,0 +1,14 @@
+var btn = $('#backtotop_button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
